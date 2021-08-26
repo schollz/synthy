c1a3f6a231e3a030ea9710e5cb409cd56a3fe29d
Zack Scholl <zack.scholl@gmail.com>
2021-08-26 11:28:34 +0700

    tweet deck

--- moomin.lua
+++ moomin.lua
@@ -1,4 +1,6 @@
-engine.name="Velvet"
+-- engine.name="Velvet"
+
+include('moomin/lib/p8')
 
 function init()
 	-- clock.run(function()
@@ -24,4 +26,45 @@ function init()
 	    end
 	  end
 	end
+
+
+	
+	clock.run(function()
+		while true do
+			clock.sleep(1/15)
+			redraw()
+		end
+	end)
+end
+
+
+pos_x,pos_y=30,30
+function enc(k,z)
+  if k==2 then
+    pos_x=pos_x+z
+  elseif k==3 then
+    pos_y=pos_y+z
+  end
+
+end
+
+-- https://twitter.com/jamesedge/status/1419289823580405768
+l,x,y,z,w={0,0,0},0,0,0,0
+function redraw()
+ screen.clear()
+ screen.level(15)
+ u,v,e,f,p,q=1,0,pos_x,pos_y,64,32
+ for g=1,3 do 
+   a=l[g]
+   c,s=cos(a),-sin(a)
+   u,v=u*c-v*s,u*s+v*c 
+   m,n=p+u*25,q+v*25
+   line(p,q,m,n)
+   circ(m,n,2)
+   l[g]=l[g]+((q-w)*x+(z-p)*y)*.0001
+   p,q=m,n 
+ end 
+ x,y,z,w=(e-p)*.1,(f-q)*.1,p,q 
+ circ(e,f,4)
+ flip()
 end
