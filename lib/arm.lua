004de6baa066825646b1442fdce42b702659af61
Zack Scholl <zack.scholl@gmail.com>
2021-08-26 19:36:08 +0700

    works

---
+++ lib/arm.lua
@@ -0,0 +1,89 @@
+local Arm={}
+
+function Arm:new(o)
+  o=o or {}
+  setmetatable(o,self)
+  self.__index=self
+  o:init(64,32)
+  return o
+end
+
+function Arm:init(x,y,u)
+  self.u=u
+  self.l={0,0,0}
+  self.x=0
+  self.y=0
+  self.z=0
+  self.w=0
+  self.base_x=x
+  self.base_y=y
+  self.points={{x,y},{x,y},{x,y}}
+  self.target={x,y}
+end
+
+-- -- https://twitter.com/jamesedge/status/1419289823580405768
+-- l,x,y,z,w={0,0,0},0,0,0,0
+-- function redraw()
+--  screen.clear()
+--  screen.level(15)
+--  u,v,e,f,p,q=1,0,pos_x,pos_y,64,32
+--  for g=1,3 do 
+--    a=l[g]
+--    c,s=cos(a),-sin(a)
+--    u,v=u*c-v*s,u*s+v*c 
+--    m,n=p+u*25,q+v*25
+--    line(p,q,m,n)
+--    circ(m,n,2)
+--    l[g]=l[g]+((q-w)*x+(z-p)*y)*.0001
+--    p,q=m,n 
+--  end 
+--  x,y,z,w=(e-p)*.1,(f-q)*.1,p,q 
+--  circ(e,f,4)
+--  flip()
+-- end
+
+function Arm:move(pos_x,pos_y)
+  self.target={pos_x,pos_y}
+  local u,v,e,f,p,q=self.u,0,pos_x,pos_y,self.base_x,self.base_y
+  for g=1,3 do
+   local a=self.l[g]
+   local c,s=math.cos(math.rad(a * 360)),math.sin(math.rad(a * 360))
+   u,v=u*c-v*s,u*s+v*c 
+   local m,n=p+u*25,q+v*25
+   self.points[g]={m,n}
+   self.l[g]=self.l[g]+((q-self.w)*self.x+(self.z-p)*self.y)*.0001
+   p,q=m,n 
+  end
+  self.x,self.y,self.z,self.w=(e-p)*.1,(f-q)*.1,p,q 
+end
+
+
+function Arm:draw()
+  screen.level(15)
+  -- local r=2
+  -- for i,point in ipairs(self.points) do
+  --   screen.move(point[1] + r, point[2])
+  --   screen.circle(point[1], point[2], r)
+  --   screen.stroke()
+  -- end
+  -- screen.move(self.base_x,self.base_y)
+  -- for i,point in ipairs(self.points) do
+  --   screen.line(point[1],point[2])
+  --   screen.stroke()
+  --   screen.move(point[1],point[2])
+  -- end
+  -- r=4
+
+  screen.move(self.base_x,self.base_y)
+  screen.curve(self.points[1][1],self.points[1][2],self.points[2][1],self.points[2][2],self.points[3][1],self.points[3][2])
+  screen.stroke()
+
+
+  return self.points
+end
+
+function Arm:coords()
+  return self.points
+end
+
+return Arm
--- moomin.lua
+++ moomin.lua
@@ -1,6 +1,7 @@
 -- engine.name="Velvet"
 
-include('moomin/lib/p8')
+-- include('moomin/lib/p8')
+articulation=include('moomin/lib/arm')
 
 function init()
 	-- clock.run(function()
@@ -28,7 +29,12 @@ function init()
 	end
 
 
-	
+	arms={}
+	arms[1]=articulation:new()
+	arms[1]:init(20,64,-1)
+	arms[2]=articulation:new()
+	arms[2]:init(128-20,64,1)
+
 	clock.run(function()
 		while true do
 			clock.sleep(1/15)
@@ -43,28 +49,86 @@ function enc(k,z)
   if k==2 then
     pos_x=pos_x+z
   elseif k==3 then
-    pos_y=pos_y+z
+    pos_y=pos_y-z
   end
 
 end
 
--- https://twitter.com/jamesedge/status/1419289823580405768
-l,x,y,z,w={0,0,0},0,0,0,0
+-- -- https://twitter.com/jamesedge/status/1419289823580405768
+-- l,x,y,z,w={0,0,0},0,0,0,0
+-- function redraw()
+--  screen.clear()
+--  screen.level(15)
+--  u,v,e,f,p,q=1,0,pos_x,pos_y,64,32
+--  for g=1,3 do 
+--    a=l[g]
+--    c,s=cos(a),-sin(a)
+--    u,v=u*c-v*s,u*s+v*c 
+--    m,n=p+u*25,q+v*25
+--    line(p,q,m,n)
+--    circ(m,n,2)
+--    l[g]=l[g]+((q-w)*x+(z-p)*y)*.0001
+--    p,q=m,n 
+--  end 
+--  x,y,z,w=(e-p)*.1,(f-q)*.1,p,q 
+--  circ(e,f,4)
+--  flip()
+-- end
+
 function redraw()
- screen.clear()
- screen.level(15)
- u,v,e,f,p,q=1,0,pos_x,pos_y,64,32
- for g=1,3 do 
-   a=l[g]
-   c,s=cos(a),-sin(a)
-   u,v=u*c-v*s,u*s+v*c 
-   m,n=p+u*25,q+v*25
-   line(p,q,m,n)
-   circ(m,n,2)
-   l[g]=l[g]+((q-w)*x+(z-p)*y)*.0001
-   p,q=m,n 
- end 
- x,y,z,w=(e-p)*.1,(f-q)*.1,p,q 
- circ(e,f,4)
- flip()
+	screen.clear()
+	local ps={}
+	local gy={}
+	for i=1,2 do
+		gy[i]={}
+		gy[i][1]=util.linlin(-1,1,-1,2,calculate_lfo(i*4,i*2))
+		gy[i][2]=util.linlin(-1,1,-1,2,calculate_lfo(i*5,i*3))
+	end
+	for i, arm in ipairs(arms) do
+		arm:move(pos_x+(i-1)*20-10+gy[i][1],pos_y+gy[i][2])
+		ps[i]=arm:draw()
+	end
+	screen.move(ps[1][3][1],ps[1][3][2])
+	screen.line(ps[2][3][1],ps[2][3][2])
+	screen.stroke()
+
+	local eyes={{ps[1][3][1]-5,ps[1][3][2]+5},{ps[2][3][1]+5,ps[2][3][2]-5}}
+	local blink=math.random()<0.01
+	for i, eye in ipairs(eyes) do
+		if blink then
+			screen.level(15)
+		  screen.circle(eye[1]-4, eye[2]-5, 6)
+		  screen.fill()
+			screen.level(15)
+			screen.circle(eye[1]-4, eye[2]-5, 6)
+			screen.stroke()
+		else
+			screen.level(0)
+			screen.circle(eye[1]-4, eye[2]-5, 6)
+			screen.fill()
+			screen.level(15)
+			screen.circle(eye[1]-4, eye[2]-5, 6)
+			screen.stroke()
+			screen.level(15)
+			screen.circle(eye[1]-(i), eye[2]-(i), 2)
+			screen.fill()
+		end
+	end
+
+	local mouth=util.linlin(-1,1,15,30,calculate_lfo(30,0))
+	screen.curve(eyes[1][1]+2,eyes[1][2]+6,pos_x,pos_y+mouth,eyes[2][1]-1,eyes[2][2]+7)
+	screen.update()
+end
+
+
+function rerun()
+  norns.script.load(norns.state.script)
+end
+
+function calculate_lfo(period,offset)
+  if period==0 then
+    return 1
+  else
+    return math.sin(2*math.pi*clock.get_beat_sec()*clock.get_beats()/period+offset)
+  end
 end
